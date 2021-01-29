ef01cbdbbbefd0386e38b8d6b95f08193fcd7dfa
OpembaMathews <opemba.m@gmail.com>
2021-01-29 12:15:15 +0300

    login_signup nav

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template ('home.html')
-
-if __name__ == '__main__':
-    app.run(debug = True)
+def home():
+   pass
+   return render_template ('home.html')
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    pass
+    return render_template ('login.html')
 
 
-@app.route('/signup/')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    pass
     return render_template ('signup.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
-
